5f48ec3227f07b7a6a23303cb6744068ffa3ad4e
Oscar Fraxedas <fraxedas@gmail.com>
2016-07-05 23:00:03 -0400

    Adding chalk

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 
+var chalk = require('chalk');
+
 process.title = 'eloqua';
 
-console.log("Hello world");
+console.error(chalk.red("Welcome to Eloqua"));
+
+var program = require('commander');
+
+program
+    .arguments('<file>')
+    .option('-u, --username <username>', 'The user to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function (file) {
+        console.log('user: %s pass: %s file: %s',
+            program.username, program.password, file);
+    })
+    .parse(process.argv);
