13fb6fc18204965b2aabb0debf1084344be2f37f
Oscar Fraxedas <fraxedas@gmail.com>
2016-07-08 16:31:20 -0400

    list apps and specific app

--- bin/eloqua-app.js
+++ bin/eloqua-app.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var eloqua = require("./eloqua");
+
+if(!eloqua.user()) return;
 
 process.title = 'eloqua-app';
 program
-    .option('-l, --list', 'List installed apps')
-    .option('-a, --apps', 'List apps developed by you')
+    .option('-l, --list', 'list apps', {isDefault: true})
+    .option('-i, --index <index>', 'list installs')
     .parse(process.argv);
+
+if (program.list) {
+    eloqua.apps();
+}
+if (program.index) {
+    eloqua.apps(program.index);
+}
+
+
--- bin/eloqua.js
+++ bin/eloqua.js
@@ -13,7 +13,7 @@
             .set('Authorization', authorization)
             .set('Accept', 'application/json')
             .end(function (err, res) {
-                if (err || res.body ==  "Not authenticated.") {
+                if (err || res.body == "Not authenticated.") {
                     console.error(chalk.red(err || res.body));
                 }
                 else {
@@ -28,15 +28,59 @@
 
     eloqua.logout = function logout() {
         var credentials = storage.getItemSync('eloqua');
-        if(credentials)
-        {
+        if (credentials) {
             console.log(chalk.green("Login out " + credentials.user.displayName));
             storage.removeItemSync('eloqua');
         }
-        else{
+        else {
             console.error(chalk.red("There is no active user"));
         }
-                    
+
+    };
+
+    eloqua.user = function user() {
+        var credentials = storage.getItemSync('eloqua');
+        if (credentials) {
+            return true;
+        }
+        else {
+            console.error(chalk.red("There is no active user. Please login."));
+            return false;
+        }
+
+    };
+
+    eloqua.apps = function apps(index) {
+        var credentials = storage.getItemSync('eloqua');
+        var offset = index || 0;
+        var limit = index ? 1 : 100;
+        request
+            .get(credentials.base + '/api/cloud/1.0/apps/configurations?offset=' + offset + '&limit=' + limit)
+            .set('Authorization', credentials.authorization)
+            .set('Accept', 'application/json')
+            .end(function (err, res) {
+                if (err) {
+                    console.error(chalk.red(err));
+                }
+                else {
+                    var apps = res.body.items;
+                    for (var i = 0; i < apps.length; i++) {
+                        var app = apps[i];
+                        if (index) {
+                            console.log(app);
+                        }
+                        else {
+                            console.log(chalk.green("[" + i + "]" + app.name));
+                        }
+                    }
+                }
+            });
+
+    };
+
+    eloqua.installs = function apps() {
+        var credentials = storage.getItemSync('eloqua');
+        console.error(chalk.red("installs"));
     };
 
 })(module.exports);
